2c2954b5a6f2ffe46efeab1f782d2e4a402fa8f4
MGavranovic <mgavranovic7@gmail.com>
2024-04-27 14:49:25 +0200

    Working on sidebar

--- src/app/sidebar/nav-data.ts
+++ src/app/sidebar/nav-data.ts
@@ -1,5 +1,5 @@
 export const navbarData = [
-  { routeLink: 'home-page', label: 'Home Page' },
-  { routeLink: '', label: 'Orders' },
-  { routeLink: '', label: 'Users' },
+  { routeLink: 'home-page', icon: 'pi pi-home', label: 'Home Page' },
+  { routeLink: '', icon: 'pi pi-shopping-bag', label: 'Orders' },
+  { routeLink: '', icon: 'pi pi-user', label: 'Users' },
 ];
