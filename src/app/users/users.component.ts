23b4f6c385d222bc4ab9821cb94db4051176d090
MGavranovic <mgavranovic7@gmail.com>
2024-04-30 12:12:36 +0200

    Added toolbar to users component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [ToolbarModule, CommonModule, ButtonModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Update',
+        icon: 'pi pi-refresh',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times',
+      },
+    ];
+  }
 }
