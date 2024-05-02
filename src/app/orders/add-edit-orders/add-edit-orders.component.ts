692dff9da90f33c4fa09c544756c9e91f1993779
MGavranovic <mgavranovic7@gmail.com>
2024-05-02 11:59:57 +0200

    Added add-edit-orders component / implemented services for  that and order component / implemented rendering, pagination, sorting of orders / Implemented adding new orders button and its functionality

---
+++ src/app/orders/add-edit-orders/add-edit-orders.component.ts
@@ -0,0 +1,95 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { Inject } from '@angular/core';
+import { OrderService } from '../../services/order.service';
+import { validateHeaderName } from 'http';
+
+@Component({
+  selector: 'app-add-edit-orders',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MultiSelectModule,
+    HttpClientModule,
+    MatTableModule,
+  ],
+  providers: [OrderService],
+  templateUrl: './add-edit-orders.component.html',
+  styleUrl: './add-edit-orders.component.css',
+})
+export class AddEditOrdersComponent implements OnInit {
+  addOrderForm: FormGroup;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _dialogRef: MatDialogRef<AddEditOrdersComponent>,
+    private _orderService: OrderService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.addOrderForm = this._fb.group({
+      title: '',
+      description: '',
+      status: '',
+      amount: '',
+      currency: '',
+      customerData: '',
+      createdOn: new Date().toString(),
+      paidOn: '',
+      authorizationCode: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.addOrderForm.patchValue(this.data);
+  }
+
+  onSubmitForm() {
+    if (this.addOrderForm.valid) {
+      if (this.data) {
+        this._orderService
+          .updateOrder(this.data.id, this.addOrderForm.value)
+          .subscribe({
+            next: (value: any) => {
+              alert('Order Updated!');
+              this._dialogRef.close(true);
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
+      } else {
+        this._orderService.addOrder(this.addOrderForm.value).subscribe({
+          next: (value: any) => {
+            this._dialogRef.close(true);
+            console.log(value);
+          },
+          error: (err: any) => {
+            console.log(err);
+          },
+        });
+      }
+    }
+  }
+}
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,14 +1,127 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
 import { ToolbarModule } from 'primeng/toolbar';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { MatTableDataSource } from '@angular/material/table';
+import { AddEditOrdersComponent } from './add-edit-orders/add-edit-orders.component';
+import { Injectable } from '@angular/core';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, DynamicDialogModule],
-  providers: [DialogService],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    CommonModule,
+    ToastModule,
+    DynamicDialogModule,
+    MatSortModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    AddEditOrdersComponent,
+
+    /*
+    ToolbarModule,
+    CommonModule,
+    ButtonModule,
+    DynamicDialogModule,
+    DialogModule,
+    ToastModule,
+    MatIconModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatButtonModule,
+    */
+  ],
+  providers: [
+    DialogService,
+    ViewChild,
+    OrderService,
+    MatSort,
+    MatPaginator,
+    MatDialog,
+  ],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css',
 })
-export class OrdersComponent {}
+export class OrdersComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'description',
+    'status',
+    'amount',
+    'currency',
+    'customerData',
+    // 'createdOn',
+    'authorizationCode',
+    'action',
+  ];
+  dataSource = new MatTableDataSource<any>(this.displayedColumns);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  constructor(private _dialog: MatDialog, private _orderService: OrderService) {
+    this.getUserList();
+  }
+
+  openOrderform(): void {
+    const dialogRef = this._dialog.open(AddEditOrdersComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (value: any) => {
+        if (value) this.getUserList();
+      },
+    });
+  }
+
+  getUserList() {
+    this._orderService.getOrdersList().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  applyFilter(e: Event) {
+    const filterValue = (e.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
