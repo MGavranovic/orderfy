10a6b867c079f550cf2988714bf24ea39ad4cbc6
MGavranovic <mgavranovic7@gmail.com>
2024-05-02 09:08:09 +0200

    Implemented search/filter in users component / Added toolbar in orders component

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [ToolbarModule, ButtonModule, DynamicDialogModule],
+  providers: [DialogService],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
 })
-export class OrdersComponent {
-
-}
+export class OrdersComponent {}
